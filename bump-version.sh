9b3694b7d4abc5eef473b862eb1a896364e79a34
asoorkeea <cvashwin@hotmail.com>
2017-12-09 20:36:26 -0500

    P2: This is a test.

--- bump-version.sh
+++ bump-version.sh
@@ -16,4 +16,16 @@ echo "Updating version to $version in all poms"
 mvn versions:set -DnewVersion=$version
 echo "Comitting version to $version in all poms"
 mvn versions:commit
-echo "Committing version changes"
+echo "Adding all poms to staging area"
+cd $working_dir/../
+git ls-files | grep 'pom.xml$' | xargs git add
+echo "Committing all poms to local branch"
+git commit -m "Updated to version $version."
+echo "Pushing all poms to remote branch master"
+git push origin master
+echo "Change completed successfully."
+
+
+
+
+
